1cb82ff162f86496d9340ce239cfded398d667fa
Wellington Carlos Massola <wellingtonmassola@outlook.com>
2021-11-26 17:24:02 -0300

    chore: updates first_project

--- ProTwo/ProTwo/urls.py
+++ ProTwo/ProTwo/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.conf.urls import url
+from django.conf.urls import include
 from django.contrib import admin
 from django.urls import path
 from AppTwo import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    path('', views.index, name="index"),
+    path('first_app/', include('first_app.urls')),
     path('admin/', admin.site.urls),
 ]
