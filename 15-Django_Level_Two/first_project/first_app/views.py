76924806b5912b1da0514e77a7f1398d2e1782d8
Wellington Carlos Massola <wellingtonmassola@outlook.com>
2021-11-29 17:39:32 -0300

    checkpoint: problem static css

--- 15-Django_Level_Two/first_project/first_app/views.py
+++ 15-Django_Level_Two/first_project/first_app/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from first_app.models import Topic, Webpage, AccessRecord
 
 # Create your views here.
 
 
 def index(request):
-    my_dict = {'insert_content': "HELLO IM FROM FIRSTAPP!"}
-    return render(request, 'first_app/index.html', context=my_dict)
+    webpage_list = AccessRecord.objects.order_by('date')
+    date_dict = {'access_records': webpage_list}
+    return render(request, 'first_app/index.html', context=date_dict)
--- 15-Django_Level_Two/first_project/first_project/settings.py
+++ 15-Django_Level_Two/first_project/first_project/settings.py
@@ -16,6 +16,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 
 # Quick-start development settings - unsuitable for production
@@ -119,6 +120,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+
+STATICFILES_DIRS = [STATIC_DIR, ]
 
 STATIC_URL = '/static/'
 
