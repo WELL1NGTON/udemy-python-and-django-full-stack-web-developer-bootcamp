a721274c2a0e88bd6f36d93735ce2878a18636dc
Wellington Carlos Massola <wellingtonmassola@outlook.com>
2021-12-02 04:47:30 -0300

    feat: registration
    Added form and logic to register new users.

--- 18-Django_Level_Five/learning_users/basic_app/forms.py
+++ 18-Django_Level_Five/learning_users/basic_app/forms.py
@@ -1,6 +1,5 @@
 from django import forms
 from django.contrib.auth.models import User
-from django.forms import fields
 from basic_app.models import UserProfileInfo
 
 
--- 18-Django_Level_Five/learning_users/basic_app/models.py
+++ 18-Django_Level_Five/learning_users/basic_app/models.py
@@ -8,7 +8,7 @@ class UserProfileInfo(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     # additional
-    portifolio_site = models.URLField(blank=True)
+    portfolio_site = models.URLField(blank=True)
 
     profile_pic = models.ImageField(upload_to='profile_pics', blank=True)
 
